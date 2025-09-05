9b39b255b9688b6d0c57e1d7ad1658fca8d4a037
David Kinkead <67561590+davidkinkead@users.noreply.github.com>
2025-09-05 23:52:14 +0100

    Create custom copy of textbook example ("companion")

---
+++ ConceptualMathematics.lean
@@ -0,0 +1,16 @@
+import VersoManual
+import ConceptualMathematics.Meta.Lean
+
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
+
+set_option pp.rawOnError true
+
+
+#doc (Manual) "A Lean 4 Companion to Conceptual Mathematics" =>
+
+%%%
+authors := ["David Kinkead"]
+%%%
+
+hello, world
