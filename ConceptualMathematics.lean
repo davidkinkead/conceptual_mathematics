bd8f117ebeea69a5575cf35c517dca6dea864390
David Kinkead <67561590+davidkinkead@users.noreply.github.com>
2025-09-08 16:21:08 +0100

    Add Lean options for Verso and organise

--- ConceptualMathematics.lean
+++ ConceptualMathematics.lean
@@ -4,8 +4,6 @@ import ConceptualMathematics.Meta.Lean
 open Verso.Genre Manual
 open Verso.Genre.Manual.InlineLean
 
-set_option pp.rawOnError true
-
 
 #doc (Manual) "A Lean 4 Companion to Conceptual Mathematics" =>
 
